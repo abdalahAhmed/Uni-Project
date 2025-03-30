6e06b96e29e5db9694048608b16d6525c1d475b7
Abdallah <abdallah01002396383@gmail.com>
2025-03-30 20:03:43 +0200

    🚀 إضافة run_migrate.py وتعديل render.yaml لتشغيل migrate تلقائيًا

--- run_migrate.py
+++ run_migrate.py
@@ -1,4 +1,10 @@
-# run_migrate.py
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "university_display.settings")
+django.setup()
+
 from django.core.management import call_command
 
-call_command('migrate')
+call_command("migrate")
+call_command("collectstatic", interactive=False, verbosity=0)
