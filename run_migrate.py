838b05bf2a42e9f79748d9ecaf0ba910c58c1b34
Abdallah <abdallah01002396383@gmail.com>
2025-03-31 02:55:28 +0200

    🔥 fix: force migrate inside buildCommand

--- run_migrate.py
+++
@@ -1,9 +0,0 @@
-import os
-import django
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "university_display.settings")
-django.setup()
-
-from django.core.management import call_command
-
-call_command("migrate")
