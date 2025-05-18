cbd673a19fbdf387fc12ecdea62869c854703fd3
Abdallaht12 <af01027958926@gmail.com>
2025-05-18 08:13:55 -0700

    ♻️ Fix urlpatterns definition and add password reset endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,4 @@
 from django.urls import path
-from .views import reset_admin_password
-
-urlpatterns += [
-    path('dev/reset-password/', reset_admin_password),
-]
-
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import (
     # تسجيل وإنشاء الحساب
@@ -22,27 +16,32 @@ from .views import (
 
     # عرض الدكاترة
     DoctorListView,
+
+    # ✅ view مؤقت لتغيير كلمة المرور
+    reset_admin_password
 )
 
 urlpatterns = [
+    # ✅ endpoint مؤقت لتحديث كلمة مرور Naif
+    path('dev/reset-password/', reset_admin_password),
+
     # التسجيل والدخول باستخدام JWT
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    #  الملف الشخصي
+    # الملف الشخصي
     path('profile/', UserProfileView.as_view(), name='user_profile'),
-    path('dev/reset-password/', reset_admin_password),
 
     # واجهات الدكتور
     path('schedule/today/', DoctorScheduleToday.as_view(), name='doctor_schedule_today'),
     path('schedule/now/', DoctorCurrentLecture.as_view(), name='doctor_current_lecture'),
-    #  واجهات المشرف (Admin)
+
+    # واجهات المشرف (Admin)
     path('admin/lectures/', AdminLectureListCreateView.as_view(), name='admin_lecture_list_create'),
     path('admin/lectures/<int:pk>/', AdminLectureDetailView.as_view(), name='admin_lecture_detail'),
     path('admin/classrooms/', AdminClassroomListView.as_view(), name='admin_classroom_list'),
 
-    # ✅ قائمة الدكاترة (لاستخدامها في تبويب Add Course مثلاً)
+    # عرض قائمة الدكاترة
     path('doctors/', DoctorListView.as_view(), name='doctor_list'),
-    
 ]
